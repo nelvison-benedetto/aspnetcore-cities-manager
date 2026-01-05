ac519aba488970434ac0f9f1f8126294beaae5c9
nelvisonbenedetto <nelvison.benedetto@gmail.com>
2026-01-05 23:37:40 +0100

    update

--- CitiesManager.Web/DatabaseContext/ApplicationDbContext.cs
+++ CitiesManager.Web/DatabaseContext/ApplicationDbContext.cs
@@ -18,7 +18,7 @@ public class ApplicationDbContext : DbContext
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         base.OnModelCreating(modelBuilder);
-        modelBuilder.Entity<City>().HasData(new City() { CityId= Guid.Parse("CDBE0726 - BAFB - 4D1F - 8B14 - 12364CF2E139"), CityName="New York" });
+        modelBuilder.Entity<City>().HasData(new City() { CityId= Guid.Parse("CDBE0726-BAFB-4D1F-8B14-12364CF2E139"), CityName="New York" });
         modelBuilder.Entity<City>().HasData(new City() { CityId = Guid.Parse("FF968DC0-1BC0-4A77-9BDC-8BDA6A91106B"), CityName="Shenzo" });
     }
     //x generare guid: tools>create guid>select 4 registry format> copy generated guid
---
+++ CitiesManager.Web/Migrations/20260105220231_DatabaseCreation.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace CitiesManager.Web.Migrations
+{
+    /// <inheritdoc />
+    public partial class DatabaseCreation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Cities",
+                columns: table => new
+                {
+                    CityId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    CityName = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cities", x => x.CityId);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Cities",
+                columns: new[] { "CityId", "CityName" },
+                values: new object[,]
+                {
+                    { new Guid("cdbe0726-bafb-4d1f-8b14-12364cf2e139"), "New York" },
+                    { new Guid("ff968dc0-1bc0-4a77-9bdc-8bda6a91106b"), "Shenzo" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Cities");
+        }
+    }
+}
