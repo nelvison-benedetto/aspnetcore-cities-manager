c41b7c726b93451814ed2abe71da9270c6d97dd1
nelvisonbenedetto <nelvison.benedetto@gmail.com>
2026-01-05 18:53:17 +0100

    start

---
+++ CitiesManager.Web/Controllers/TestController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CitiesManager.Web.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class TestController : ControllerBase
+{
+    [HttpGet]
+    public string myMethod() {
+        return "hello nextworld";
+    }
+
+}
